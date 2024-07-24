b16ca73c39f1f5a57e20d2086ea10540efad1004
phonsokpheaktra <phonsokpheaktra18@kit.edu.kh>
2024-07-24 10:46:14 +0700

    Reg_admin_1

--- reg_admin_1.spec.ts
+++ reg_admin_1.spec.ts
@@ -1,14 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('Login to Admin Page with a valid Username & Password', async ({ page }) => {
-  await page.goto('https://stg-admin.irodoki.com/login')
 
-  await page.getByPlaceholder('施設または先生のアカウントID').fill('account')
-  await page.getByPlaceholder('パスワード（最低8文字）').fill('password')
-  await page.click("//button[text()= 'ログイン']")
-
-  await expect(page).toHaveURL(new RegExp('https://exaple.com/'))
-
-  page.close()
-
+  async function name() {
+    await page.goto('https://stg-admin.irodoki.com/login')
+    await page.getByPlaceholder('施設または先生のアカウントID').fill('account')
+    await page.getByPlaceholder('パスワード（最低8文字）').fill('password')
+    await page.click("//button[text()= 'ログイン']")
+    await expect(page).toHaveURL(new RegExp('https://example.com/'))
+    page.close()
+    }
 })
